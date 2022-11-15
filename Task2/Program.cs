fcb0adbb0f8e8832ec867804f280d54fbeb64c45
Uri_Klevcov <a-08-137.mail.ru>
2022-11-15 20:35:22 +0500

    Task2 amount positive numbers

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+double Prompt(string message)
+{
+    System.Console.Write(message + " > ");
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double[] UserArray(int userAmount)
+{
+    double[] array = new double[userAmount];
+    array[0] = Prompt("Начните вводить любые числа");
+    for (int i = 1; i < userAmount; i++)
+    {
+        array[i] = Prompt("и ещё");
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    int length = array.Length;
+    System.Console.Write(array[0]);
+    for (int i = 1; i < length; i++)
+    {
+        System.Console.Write($"; {array[i]}");
+    }
+}
+
+int AmountPositiveNumbers(double[] array)
+{
+    int amountNumbers = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) amountNumbers++;
+    }
+    return amountNumbers;
+}
+
+int amount = Convert.ToInt32(Prompt("Укажите количество чисел, на ввод которых Вы готовы потратить время"));
+double[] numbers = UserArray(amount);
+System.Console.WriteLine("И так, подытожим, Вы ввели следующие значения:");
+PrintArray(numbers);
+System.Console.WriteLine();
+System.Console.WriteLine($"из них {AmountPositiveNumbers(numbers)} положительных");
+
+
+
